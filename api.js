d5a70282c3104777bc6313a780d5295a005e3bdd
Tshweu <t.t.sephiri@gmail.com>
2020-10-12 18:45:07 +0200

    added registration endpoint sending user to db

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 // This file will be used to route all the requests 
 const express = require('express');
 const  mongoose  = require('mongoose');
+const User = require('./models/user');
 const router = express.Router();
 
 const uri = "mongodb+srv://user1:Y6RHjust7IUxw1oE@metadata.s6ori.mongodb.net/DataClassification?retryWrites=true&w=majority";
@@ -15,6 +16,18 @@ mongoose.connect(uri,{ useNewUrlParser: true } ,(err)=>{
 
 router.get('/',(req,res)=>{
     res.send('API router');
+})
+
+router.post('/register',(req,res)=>{
+    let userData = req.body;
+    let user = new User(userData);
+    user.save((err,registeredUser)=>{
+        if(err){
+            console.error("ERROR, Failed to save user : ",err);
+        }else{
+            res.status(200).send(registeredUser);
+        }
+    })
 })
 
 module.exports = router;
