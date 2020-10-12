1481f723953a4f9e2ca6ad03b4a0ec1a6303cc7e
Tshweu <t.t.sephiri@gmail.com>
2020-10-12 18:02:47 +0200

    imported in server

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
+// api router ref
+const api = require('./api');
 
-//to allow our app to recieve json 
+//to allow our app to recieve json data
 app.use(bodyParser.json());
+//telling app to use it
+// when api is specified in the route
+app.use('/api',api);
+
+
 
 app.get('/',(req,res)=>{
     res.send("Data catalog API");
