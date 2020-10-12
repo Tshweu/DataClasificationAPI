de74483f670c2fcef45ad5315b5da01be3ea0583
Tshweu <t.t.sephiri@gmail.com>
2020-10-12 18:38:05 +0200

    added connection to remote database

--- api.js
+++ api.js
@@ -1,6 +1,17 @@
 // This file will be used to route all the requests 
 const express = require('express');
+const  mongoose  = require('mongoose');
 const router = express.Router();
+
+const uri = "mongodb+srv://user1:Y6RHjust7IUxw1oE@metadata.s6ori.mongodb.net/DataClassification?retryWrites=true&w=majority";
+
+mongoose.connect(uri,{ useNewUrlParser: true } ,(err)=>{
+    if(err){
+        console.error("Database connection error : ",err);
+    }else{
+        console.log("connected to database");
+    }
+})
 
 router.get('/',(req,res)=>{
     res.send('API router');
